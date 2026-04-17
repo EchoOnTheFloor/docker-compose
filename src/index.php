5b595fcb049292980f60d1d2938483ef2c35b3cb
glu <lightlinks010@gmail.com>
2026-04-17 13:54:33 +0200

    DEBUG SELinux problem

--- src/index.php
+++ src/index.php
@@ -1,26 +1,20 @@
 <?php
-echo "<h1Test de la Stack Docker Compose</h1";
 // 1. Test PHP
-echo "<p>
-✅
- PHP fonctionne Version " . phpversion() . ")</p>";
+echo "✅ PHP fonctionne (Version " . phpversion() . ")<br>";
+
 // 2. Test Connexion MySQL
-// Attention : 'db' est le nom du service dans docker-compose.yml !
 $servername = "db"; 
-$username = "user";     // Vos identifiants du docker-compose
+$username = "user";     
 $password = "password"; 
 $dbname = "company";
+
 // Création de la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Vérification
-if $conn→connect_error) {
-die("<p style='color:red'>
-❌
- Echec de la connexion MySQL  " . $conn→c
-onnect_error . "</p>");
+if ($conn->connect_error) {
+    die("❌ Echec de la connexion MySQL : " . $conn->connect_error);
 }
-echo "<p style='color:green'>
-✅
- Succès ! PHP est connecté à MySQL.</p
->";
+
+echo "✅ Succès ! PHP est connecté à MySQL.";
 ?>
